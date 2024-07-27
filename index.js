eb4d44bacb996b93fd051ed5cf0d818d6932bde3
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-07-27 18:11:49 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const fs = require("fs");
 
+app.use(cors());
 app.use(bodyParser.json({
   limit: "50mb"
 }));
